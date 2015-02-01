5fdf7ff8c819ac61bfe1675bf8f44fa0fc762508
Ethan Yue Zhao <xishanlake@gmail.com>
2015-02-01 12:29:02 -0500

    ordered by last name

--- HealthApp/Public/Patients/Scripts/app.js
+++ HealthApp/Public/Patients/Scripts/app.js
@@ -6,31 +6,10 @@ healthApp.controller('PatientListCtrl', ['$scope', '$http',
     function ($scope, $http) {        
         $http.get('patients/data/patientList.json').success(function (data) {
             $scope.patientList = data;
-        });   
+            $scope.orderProp = 'ln';
+        });
+        
     }]);
 
-//phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-//  function ($scope, $http) {
-//        $http.get('phones/phones.json').success(function (data) {
-//            $scope.phones = data;
-//        });
-        
-//        $scope.orderProp = 'age';
-//    }]);
-//healthApp.controller('PatientListCtrl', function ($scope) {
-//    $scope.patientList = [
-//        {
-//            'id': 'Nexus S',
-//            'fn': 'Fast just got faster with Nexus S.'
-//        },
-//        {
-//            'id': 'Motorola XOOM™ with Wi-Fi',
-//            'fn': 'The Next, Next Generation tablet.'
-//        },
-//        {
-//            'id': 'MOTOROLA XOOM™',
-//            'fn': 'The Next, Next Generation tablet.'
-//        }
-//    ];
-//});
+
 
