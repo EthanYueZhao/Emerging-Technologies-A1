e566454cb95e26a8643254c48ebfefca40f59c98
Ethan Yue Zhao <xishanlake@gmail.com>
2015-01-31 21:39:55 -0500

    http server works

--- HealthApp/Public/Patients/Scripts/app.js
+++ HealthApp/Public/Patients/Scripts/app.js
@@ -1,1 +1,10 @@
-﻿
+﻿var healthApp = angular.module('healthApp', []);
+
+healthApp.controller('PatientListCtrl', function ($scope) {
+    $http.get('Public/Patients/Data/Patient.json').success(function (data) {
+        $scope.patientList = data;
+    });
+    
+   // $scope.orderProp = 'age';
+});
+
--- HealthApp/server.js
+++ HealthApp/server.js
@@ -1,6 +1,18 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (request, response) {
+    response.sendfile("index.html");
+});
+
+app.listen(port);
