f5a0d1b28cf8a0053913553b2e6441807e29f5cb
Ethan Yue Zhao <xishanlake@gmail.com>
2015-01-31 23:03:27 -0500

    displayed patients from Json file

--- HealthApp/Public/Patients/Scripts/app.js
+++ HealthApp/Public/Patients/Scripts/app.js
@@ -1,10 +1,36 @@
-﻿var healthApp = angular.module('healthApp', []);
+﻿'use strict';
 
-healthApp.controller('PatientListCtrl', function ($scope) {
-    $http.get('Public/Patients/Data/Patient.json').success(function (data) {
-        $scope.patientList = data;
-    });
-    
-   // $scope.orderProp = 'age';
-});
+var healthApp = angular.module('healthApp', []);
+
+healthApp.controller('PatientListCtrl', ['$scope', '$http', 
+    function ($scope, $http) {        
+        $http.get('patients/data/patientList.json').success(function (data) {
+            $scope.patientList = data;
+        });   
+    }]);
+
+//phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+//  function ($scope, $http) {
+//        $http.get('phones/phones.json').success(function (data) {
+//            $scope.phones = data;
+//        });
+        
+//        $scope.orderProp = 'age';
+//    }]);
+//healthApp.controller('PatientListCtrl', function ($scope) {
+//    $scope.patientList = [
+//        {
+//            'id': 'Nexus S',
+//            'fn': 'Fast just got faster with Nexus S.'
+//        },
+//        {
+//            'id': 'Motorola XOOM™ with Wi-Fi',
+//            'fn': 'The Next, Next Generation tablet.'
+//        },
+//        {
+//            'id': 'MOTOROLA XOOM™',
+//            'fn': 'The Next, Next Generation tablet.'
+//        }
+//    ];
+//});
 
--- HealthApp/server.js
+++ HealthApp/server.js
@@ -8,9 +8,20 @@ var port = process.env.port || 1337;
 
 
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+
+
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use("/datas", express.static(__dirname + 'patients/public/data'));
+//app.use("/scripts", express.static(__dirname + '/public/javascripts'));
+//app.use("/images", express.static(__dirname + '/public/images'));
+
+
+
 app.get('/', function (request, response) {
     response.sendfile("index.html");
 });
